3b7f077d86bf0e11daf138c04d1bb817c0f20a15
Erick Wendel <erick.workspace@gmail.com>
2018-11-27 10:07:44 -0200

    ortographic error
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- components/AboutCourse.vue
+++ components/AboutCourse.vue
@@ -2,12 +2,26 @@
   <section class="section AboutCourse">
     <div class="container AboutCourseContainer">
       <StyledTitle margin="0px 0px 30px 0px">Sobre o curso</StyledTitle>
-      <p>
-        Varrendo a internet, encontramos diversos treinamentos sobre <strong>Node.js</strong> e <strong>Javascript</strong>. Mas na maioria das vezes estes conteúdos <strong>são focados na mesma coisa</strong>! A grande maioria fala sobre a Stack <strong>MEAN</strong> (MongoDB, Express.js, Angular.js e Node.js), programação para Web ou apenas <strong>simples CRUDs</strong> (cinco operações básicas em programação), coisas que para <strong>estudos</strong> funcionam muito bem, mas a <strong>realidade</strong> no dia a dia de desenvolvimento de <strong>aplicações</strong> focado em <strong>produção</strong> é um pouco <strong>diferente</strong>.
+      <p>Varrendo a internet, encontramos diversos treinamentos sobre
+        <strong>Node.js</strong> e
+        <strong>Javascript</strong>. Mas na maioria das vezes estes conteúdos
+        <strong>são focados na mesma coisa</strong>! A grande maioria fala sobre a Stack
+        <strong>MEAN</strong> (MongoDB, Express.js, Angular.js e Node.js), programação para Web ou apenas
+        <strong>simples CRUDs</strong> (as operações básicas de sistemas: Cadastrar, Listar, Atualizar e Remover), coisas que para
+        <strong>estudos</strong> funcionam muito bem, mas a
+        <strong>realidade</strong> no dia a dia de desenvolvimento de
+        <strong>aplicações</strong> focado em
+        <strong>produção</strong> é um pouco
+        <strong>diferente</strong>.
       </p>
       <br>
       <p>
-        Este treinamento foi <strong>desenvolvido</strong> sob medida <strong>baseado</strong> em uma pesquisa sobre o que a <strong>comunidade brasileira mais necessita</strong>, suas principais <strong>dificuldades</strong> e <strong>desafios</strong>.
+        Este treinamento foi
+        <strong>desenvolvido</strong> sob medida
+        <strong>baseado</strong> em uma pesquisa sobre o que a
+        <strong>comunidade brasileira mais necessita</strong>, suas principais
+        <strong>dificuldades</strong> e
+        <strong>desafios</strong>.
         Neste curso você aprenderá desde o básico de Javascript, ciclo de vida, manipulação de listas ao desenvolvimento real de aplicações em ambiente de produção. Na prática você também aprenderá conceitos sobre desenvolvimento orientado a testes, autenticação, autorização, documentação de serviços e gerenciamento de variáveis de ambiente (environments) de aplicações.
       </p>
     </div>
@@ -15,17 +29,16 @@
 </template>
 
 <script>
-import StyledTitle from './Title'
+import StyledTitle from "./Title";
 
 export default {
-  name: 'AboutCourse',
+  name: "AboutCourse",
   components: { StyledTitle }
-}
+};
 </script>
 
 <style lang="scss" scoped>
 .AboutCourse {
-
   > .AboutCourseContainer {
     display: flex;
     flex-direction: column;
